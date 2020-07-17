b0f820ba6a77fcaacc980a0d72c8eca2e4d8a5fd
Ricardo <rirova81@gmail.com>
2020-07-17 19:31:52 +0200

    Add horizontal bar chart
    Improvements:
    Ceate function scope to avoid collisions names

---
+++ js/horizontal-bar-script.js
@@ -0,0 +1,151 @@
+(function () {
+  let DATA_COUNT = 50;
+
+  let data = [];
+
+  for (let i = 0; i < DATA_COUNT; i++) {
+    let datum = {};
+    datum.label = `Data ${i + 1}`;
+    datum.value = Math.floor(Math.random() * 800);
+    data.push(datum);
+  }
+
+  let margin = { top: 20, right: 0, bottom: 20, left: 60 };
+  let marginOverview = 10;
+  let selectorWidth = 40;
+  let widthOverview = selectorWidth + marginOverview;
+  let width = 600 - margin.left - margin.right - widthOverview;
+  let height = 400 - margin.top - margin.bottom;
+
+  let maxLength = d3.max(data.map((d) => d.label.length));
+  let barWidth = maxLength * 7;
+  let numBars = Math.round(width / barWidth);
+  let isScrollDisplayed = barWidth * data.length > width;
+
+  const slicedData = data.slice(0, numBars).reverse();
+
+  let xscale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.value)])
+    .range([0, width]);
+
+  let yscale = d3
+    .scaleBand()
+    .domain(slicedData.map((d) => d.label))
+    .range([height, 0])
+    .padding(0.1);
+
+  let svg = d3
+    .select('#horizontal-chart')
+    .append('svg')
+    .attr('width', width + margin.left + widthOverview)
+    .attr('height', height + margin.top + margin.bottom);
+
+  let diagram = svg
+    .append('g')
+    .attr('transform', `translate(${margin.left} , ${margin.top})`);
+
+  diagram
+    .append('g')
+    .attr('class', 'x axis')
+    .attr('transform', `translate(0, 0)`)
+    .call(d3.axisTop(xscale));
+
+  diagram.append('g').attr('class', 'y axis').call(d3.axisLeft(yscale));
+
+  let bars = diagram.append('g');
+
+  bars
+    .selectAll('rect')
+    .data(data.slice(0, numBars), (d) => d.label)
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', 0)
+    .attr('y', (d) => yscale(d.label))
+    .attr('width', (d) => xscale(d.value))
+    .attr('height', yscale.bandwidth());
+
+  if (isScrollDisplayed) {
+    const dataReversed = data.slice(0, data.length).reverse();
+
+    let xOverview = d3
+      .scaleLinear()
+      .range([0, selectorWidth])
+      .domain(xscale.domain());
+
+    let yOverview = d3
+      .scaleBand()
+      .domain(dataReversed.map((d) => d.label))
+      .range([height, 0])
+      .padding(0.2);
+
+    let subBars = diagram.selectAll('.subBar').data(dataReversed);
+
+    subBars
+      .enter()
+      .append('rect')
+      .classed('subBar', true)
+      .attr('height', () => yOverview.bandwidth())
+      .attr('width', (d) => xOverview(d.value))
+      .attr('x', width + marginOverview)
+      .attr('y', (d) => yOverview(d.label));
+
+    var displayedH = d3
+      .scaleQuantize()
+      .domain([0, height])
+      .range(d3.range(data.length));
+
+    diagram
+      .append('rect')
+      .attr('transform', `translate(${width + marginOverview}, 0)`)
+      .attr('class', 'mover')
+      .attr('x', 0)
+      .attr('y', 0)
+      .attr('height', 100)
+      .attr('width', selectorWidth)
+      .attr('pointer-events', 'all')
+      .attr('cursor', 'ns-resize')
+      .call(d3.drag().on('drag', display));
+  }
+
+  function display() {
+    let y = parseInt(d3.select(this).attr('y')),
+      ny = y + d3.event.dy,
+      h = parseInt(d3.select(this).attr('height')),
+      f,
+      nf,
+      new_data,
+      rects;
+
+    if (ny < 0 || ny + h > height + margin.top + margin.bottom) return;
+
+    d3.select(this).attr('y', ny);
+
+    f = displayedH(y);
+    nf = displayedH(ny);
+
+    if (f === nf) return;
+
+    new_data = data.slice(nf, nf + numBars).reverse();
+
+    yscale.domain(new_data.map((d) => d.label));
+
+    diagram.select('.y.axis').call(d3.axisLeft(yscale));
+
+    rects = bars.selectAll('rect').data(new_data, (d) => d.value);
+
+    rects.attr('y', (d) => yscale(d.label));
+
+    rects
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', 0)
+      .attr('y', (d) => yscale(d.label))
+      .attr('width', (d) => xscale(d.value))
+      .attr('height', yscale.bandwidth());
+
+    rects.exit().remove();
+  }
+})();
--- js/vertical-bar-script.js
+++ js/vertical-bar-script.js
@@ -1,139 +1,75 @@
 // Adapted from http://bl.ocks.org/cse4qf/95c335c73af588ce48646ac5125416c6
 
-var DATA_COUNT = 50;
+(function () {
+  let DATA_COUNT = 50;
 
-var data = [];
+  let data = [];
 
-for (var i = 0; i < DATA_COUNT; i++) {
-  var datum = {};
-  datum.label = `Data ${i + 1}`;
-  datum.value = Math.floor(Math.random() * 600);
-  data.push(datum);
-}
+  for (let i = 0; i < DATA_COUNT; i++) {
+    let datum = {};
+    datum.label = `Data ${i + 1}`;
+    datum.value = Math.floor(Math.random() * 600);
+    data.push(datum);
+  }
 
-var margin = { top: 20, right: 10, bottom: 20, left: 40 };
-var marginOverview = { top: 30, right: 10, bottom: 20, left: 40 };
-var selectorHeight = 40;
-var width = 600 - margin.left - margin.right;
-var height = 400 - margin.top - margin.bottom - selectorHeight;
-var heightOverview = 80 - marginOverview.top - marginOverview.bottom;
+  let margin = {
+    top: 20,
+    right: 10,
+    bottom: 20,
+    left: 40,
+  };
+  let marginOverview = {
+    top: 30,
+    right: 10,
+    bottom: 20,
+    left: 40,
+  };
+  let selectorHeight = 40;
+  let width = 600 - margin.left - margin.right;
+  let height = 400 - margin.top - margin.bottom - selectorHeight;
+  let heightOverview = 80 - marginOverview.top - marginOverview.bottom;
 
-var maxLength = d3.max(data.map((d) => d.label.length));
-var barWidth = maxLength * 7;
-var numBars = Math.round(width / barWidth);
-var isScrollDisplayed = barWidth * data.length > width;
+  let maxLength = d3.max(data.map((d) => d.label.length));
+  let barWidth = maxLength * 7;
+  let numBars = Math.round(width / barWidth);
+  let isScrollDisplayed = barWidth * data.length > width;
 
-console.log(isScrollDisplayed);
+  console.log(isScrollDisplayed);
 
-var xscale = d3
-  .scaleBand()
-  .domain(data.slice(0, numBars).map((d) => d.label))
-  .range([0, width])
-  .padding(0.1);
-
-var yscale = d3
-  .scaleLinear()
-  .domain([0, d3.max(data, (d) => d.value)])
-  .range([height, 0]);
-
-var svg = d3
-  .select('#vertical-chart')
-  .append('svg')
-  .attr('width', width + margin.left + margin.right)
-  .attr('height', height + margin.top + margin.bottom + selectorHeight);
-
-var diagram = svg
-  .append('g')
-  .attr('transform', `translate(${margin.left} , ${margin.top})`);
-
-diagram
-  .append('g')
-  .attr('class', 'x axis')
-  .attr('transform', `translate(0, ${height})`)
-  .call(d3.axisBottom(xscale));
-
-diagram.append('g').attr('class', 'y axis').call(d3.axisLeft(yscale));
-
-var bars = diagram.append('g');
-
-bars
-  .selectAll('rect')
-  .data(data.slice(0, numBars), (d) => d.label)
-  .enter()
-  .append('rect')
-  .attr('class', 'bar')
-  .attr('x', (d) => xscale(d.label))
-  .attr('y', (d) => yscale(d.value))
-  .attr('width', xscale.bandwidth())
-  .attr('height', (d) => height - yscale(d.value));
-
-if (isScrollDisplayed) {
-  var xOverview = d3
+  let xscale = d3
     .scaleBand()
-    .domain(data.map((d) => d.label))
+    .domain(data.slice(0, numBars).map((d) => d.label))
     .range([0, width])
-    .padding(0.2);
+    .padding(0.1);
 
-  yOverview = d3.scaleLinear().range([heightOverview, 0]);
-  yOverview.domain(yscale.domain());
+  let yscale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.value)])
+    .range([height, 0]);
 
-  var subBars = diagram.selectAll('.subBar').data(data);
+  let svg = d3
+    .select('#vertical-chart')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom + selectorHeight);
 
-  subBars
-    .enter()
-    .append('rect')
-    .classed('subBar', true)
-    .attr('height', (d) => heightOverview - yOverview(d.value))
-    .attr('width', () => xOverview.bandwidth())
-    .attr('x', (d) => xOverview(d.label))
-    .attr('y', (d) => height + heightOverview + yOverview(d.value));
-
-  var displayed = d3
-    .scaleQuantize()
-    .domain([0, width])
-    .range(d3.range(data.length));
+  let diagram = svg
+    .append('g')
+    .attr('transform', `translate(${margin.left} , ${margin.top})`);
 
   diagram
-    .append('rect')
-    .attr('transform', 'translate(0, ' + (height + margin.bottom) + ')')
-    .attr('class', 'mover')
-    .attr('x', 0)
-    .attr('y', 0)
-    .attr('height', selectorHeight)
-    .attr('width', Math.round(parseFloat(numBars * width) / data.length))
-    .attr('pointer-events', 'all')
-    .attr('cursor', 'ew-resize')
-    .call(d3.drag().on('drag', display));
-}
+    .append('g')
+    .attr('class', 'x axis')
+    .attr('transform', `translate(0, ${height})`)
+    .call(d3.axisBottom(xscale));
 
-function display() {
-  var x = parseInt(d3.select(this).attr('x')),
-    nx = x + d3.event.dx,
-    w = parseInt(d3.select(this).attr('width')),
-    f,
-    nf,
-    new_data,
-    rects;
+  diagram.append('g').attr('class', 'y axis').call(d3.axisLeft(yscale));
 
-  if (nx < 0 || nx + w > width) return;
+  let bars = diagram.append('g');
 
-  d3.select(this).attr('x', nx);
-
-  f = displayed(x);
-  nf = displayed(nx);
-
-  if (f === nf) return;
-
-  new_data = data.slice(nf, nf + numBars);
-
-  xscale.domain(new_data.map((d) => d.label));
-  diagram.select('.x.axis').call(d3.axisBottom(xscale));
-
-  rects = bars.selectAll('rect').data(new_data, (d) => d.label);
-
-  rects.attr('x', (d) => xscale(d.label));
-
-  rects
+  bars
+    .selectAll('rect')
+    .data(data.slice(0, numBars), (d) => d.label)
     .enter()
     .append('rect')
     .attr('class', 'bar')
@@ -142,5 +78,81 @@ function display() {
     .attr('width', xscale.bandwidth())
     .attr('height', (d) => height - yscale(d.value));
 
-  rects.exit().remove();
-}
+  if (isScrollDisplayed) {
+    let xOverview = d3
+      .scaleBand()
+      .domain(data.map((d) => d.label))
+      .range([0, width])
+      .padding(0.2);
+
+    yOverview = d3.scaleLinear().range([heightOverview, 0]);
+    yOverview.domain(yscale.domain());
+
+    let subBars = diagram.selectAll('.subBar').data(data);
+
+    subBars
+      .enter()
+      .append('rect')
+      .classed('subBar', true)
+      .attr('height', (d) => heightOverview - yOverview(d.value))
+      .attr('width', () => xOverview.bandwidth())
+      .attr('x', (d) => xOverview(d.label))
+      .attr('y', (d) => height + heightOverview + yOverview(d.value));
+
+    var displayedV = d3
+      .scaleQuantize()
+      .domain([0, width])
+      .range(d3.range(data.length));
+
+    diagram
+      .append('rect')
+      .attr('transform', 'translate(0, ' + (height + margin.bottom) + ')')
+      .attr('class', 'mover')
+      .attr('x', 0)
+      .attr('y', 0)
+      .attr('height', selectorHeight)
+      .attr('width', Math.round(parseFloat(numBars * width) / data.length))
+      .attr('pointer-events', 'all')
+      .attr('cursor', 'ew-resize')
+      .call(d3.drag().on('drag', display));
+  }
+
+  function display() {
+    let x = parseInt(d3.select(this).attr('x')),
+      nx = x + d3.event.dx,
+      w = parseInt(d3.select(this).attr('width')),
+      f,
+      nf,
+      new_data,
+      rects;
+
+    if (nx < 0 || nx + w > width) return;
+
+    d3.select(this).attr('x', nx);
+
+    f = displayedV(x);
+    nf = displayedV(nx);
+
+    if (f === nf) return;
+
+    new_data = data.slice(nf, nf + numBars);
+
+    xscale.domain(new_data.map((d) => d.label));
+    diagram.select('.x.axis').call(d3.axisBottom(xscale));
+
+    rects = bars.selectAll('rect').data(new_data, (d) => d.label);
+
+    rects.attr('x', (d) => xscale(d.label));
+
+    rects
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', (d) => xscale(d.label))
+      .attr('y', (d) => yscale(d.value))
+      .attr('width', xscale.bandwidth())
+      .attr('height', (d) => height - yscale(d.value));
+
+    rects.exit().remove();
+  }
+})();
